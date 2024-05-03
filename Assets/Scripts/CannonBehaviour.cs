d5e4878dcd03af5242f91d484bf1884d75de0b00
EdBarton2 <edward2.barton@live.uwe.ac.uk>
2024-05-04 00:06:38 +0100

    Initial commit

---
+++ Assets/Scripts/CannonBehaviour.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBehaviour : MonoBehaviour
+{
+    public AudioClip cannonShot;
+    public ParticleSystem smoke;
+    public GameObject ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ShowUI();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ShootCannon();
+            }
+
+           
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+        HideUI();
+    }
+    public void HideUI()
+    {
+        ui.SetActive(false);
+    }
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+    }
+    public void ShootCannon()
+    {
+        smoke.Play();
+        gameObject.GetComponent<AudioSource>().PlayOneShot(cannonShot);
+    }
+}
